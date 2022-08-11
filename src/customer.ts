5709b75e736a7775f0dbd517a51bd507cd441c3d
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-11 09:59:55 -0300

    feat: add methods changeName, activate and deactivate

--- src/customer.ts
+++ src/customer.ts
@@ -2,30 +2,24 @@ class Customer {
     _id: string;
     _name: string;
     _address: string;
+    _active: boolean;
 
     constructor(id: string, name: string, address: string) {
         this._id = id;
         this._name = name;
         this._address = address;
+        this._active = true;
     }
 
-    get id(): string {
-        return this._id;
-    }
-
-    get name(): string {
-        return this._name;
-    }
-
-    get address(): string {
-        return this._address;
-    }
-
-    set name(name: string) {
+    changeName(name: string) {
         this._name = name;
     }
 
-    set address(address: string) {
-        this._address = address;
+    activate() {
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
     }
 }
