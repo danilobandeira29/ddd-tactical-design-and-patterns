0db294ab2f0b5e5446596dd45a9e950f3cc7fa92
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-13 11:51:37 -0300

    test: should throw error when name is empty

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -4,4 +4,8 @@ describe("Customer unit tests", () => {
     it("should throw error when id is empty", () => {
         expect(() => new Customer("", "John")).toThrowError("Id is required");
     })
+
+    it("should throw error when name is empty", () => {
+        expect(() => new Customer("123", "")).toThrowError("Name is required");
+    })
 })
