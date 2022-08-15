a1c029647fbbf36e5b7f3ac2d1da3254bb8791bc
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-15 09:59:20 -0300

    test: should throw error when price is less than zero

--- src/entity/product.spec.ts
+++ src/entity/product.spec.ts
@@ -8,4 +8,8 @@ describe("Product unit tests", () => {
     it("should throw error when name is empty", () => {
         expect(() => new Product("123", "", 100)).toThrowError("Name is required");
     })
+
+    it("should throw error when price is less than zero", () => {
+        expect(() => new Product("123", "Product 1", -1)).toThrowError("Price must be greater than zero");
+    })
 })
