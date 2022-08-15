af3e06d6616f7499aed737ad8f4158a49048a09b
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-15 11:02:15 -0300

    test: should throw error when trying to create new Item and quantity is zero

--- src/entity/order_item.spec.ts
+++ src/entity/order_item.spec.ts
@@ -4,4 +4,8 @@ describe("Order unit tests", () => {
     it("should throw error when trying to create new Item and quantity is less than zero", () => {
         expect(() => new OrderItem("1", "Item 1", 100, "123", -1)).toThrowError("Quantity must be greater than zero");
     })
+
+    it("should throw error when trying to create new Item and quantity is zero", () => {
+        expect(() => new OrderItem("1", "Item 1", 100, "123", 0)).toThrowError("Quantity must be greater than zero");
+    })
 })
