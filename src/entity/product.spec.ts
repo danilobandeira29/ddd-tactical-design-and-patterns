c72abfa019a78958b2f990695b5d5c37ead10913
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-15 10:07:23 -0300

    test: should throw error when trying to change name and name is empty

--- src/entity/product.spec.ts
+++ src/entity/product.spec.ts
@@ -22,4 +22,9 @@ describe("Product unit tests", () => {
         product.changeName("Product 2");
         expect(product.name).toEqual("Product 2");
     })
+
+    it("should throw error when trying to change name and name is empty", () => {
+        const product = new Product("123", "Product 1", 100);
+        expect(() => product.changeName("")).toThrowError("Name is required")
+    })
 })
