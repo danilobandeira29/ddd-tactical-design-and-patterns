5077d5cf594663e2edf98ad89852925c291baf6f
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-15 11:03:12 -0300

    style: test message

--- src/entity/order_item.spec.ts
+++ src/entity/order_item.spec.ts
@@ -1,11 +1,11 @@
 import OrderItem from "./order_item";
 
 describe("Order unit tests", () => {
-    it("should throw error when trying to create new Item and quantity is less than zero", () => {
+    it("should throw error when trying to create and quantity is less than zero", () => {
         expect(() => new OrderItem("1", "Item 1", 100, "123", -1)).toThrowError("Quantity must be greater than zero");
     })
 
-    it("should throw error when trying to create new Item and quantity is zero", () => {
+    it("should throw error when trying to create and quantity is zero", () => {
         expect(() => new OrderItem("1", "Item 1", 100, "123", 0)).toThrowError("Quantity must be greater than zero");
     })
 })
