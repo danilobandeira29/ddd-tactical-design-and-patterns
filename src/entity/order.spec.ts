44335a37d7d9aa442e087b05dabfb1b9e60f5c49
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-15 09:35:18 -0300

    test: should calculate total of 40 which is an item with a value of 35 and another item with a value of 5

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -1,4 +1,5 @@
 import Order from "./order";
+import OrderItem from "./order_item";
 
 describe("Order unit tests", () => {
     it("should throw error when id is empty", () => {
@@ -11,5 +12,12 @@ describe("Order unit tests", () => {
 
     it("should throw error when order items are empty", () => {
         expect(() => new Order("1234", "123", [])).toThrowError("Items quantity must be greater than zero");
+    })
+
+    it("should calculate total of 40 which is an item with a value of 35 and another item with a value of 5", () => {
+        const item1 = new OrderItem("1", "Item 1", 35);
+        const item2 = new OrderItem("2", "Item 2", 5);
+        const order = new Order("123", "1234", [item1, item2]);
+        expect(order.total()).toEqual(40);
     })
 })
