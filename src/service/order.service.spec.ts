a112304920f1796717f25d5fdc738f0f4f8c1e93
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-16 11:09:05 -0300

    test: should reward a customer with two hundred points of an order with total of four hundred

--- src/service/order.service.spec.ts
+++ src/service/order.service.spec.ts
@@ -1,8 +1,17 @@
 import OrderItem from "../entity/order_item";
 import Order from "../entity/order";
 import OrderService from "./order.service";
+import Customer from "../entity/customer";
 
 describe("Order service unit tests", () => {
+    it("should reward a customer with two hundred points of an order with total of four hundred", () => {
+        const customer = new Customer("1", "Danilo");
+        const item1 = new OrderItem("1", "Item 1", 50, "123", 2);
+        const item2 = new OrderItem("2", "Item 2", 150, "123", 2);
+        OrderService.placeOrder(customer, [item1, item2]);
+        expect(customer.rewardPoints).toEqual(200);
+    })
+
     it("should calculate total of all orders", () => {
         const item1 = new OrderItem("1", "Item 1", 100, "123", 2);
         const item2 = new OrderItem("2", "Item 2", 200, "1234", 4);
