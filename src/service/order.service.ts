33fb11e9b96a4b405dc0ca0da178754d06c44651
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-16 11:08:35 -0300

    feat: add place order method

--- src/service/order.service.ts
+++ src/service/order.service.ts
@@ -1,6 +1,15 @@
 import Order from "../entity/order";
+import Customer from "../entity/customer";
+import OrderItem from "../entity/order_item";
+import { v4 as uuid } from "uuid";
 
 export default class OrderService {
+    static placeOrder(customer: Customer, items: Array<OrderItem>): Order {
+        const order = new Order(uuid(), customer.id, items);
+        customer.addRewardPoints(order.total() / 2);
+        return order;
+    }
+
     static total(orders: Array<Order>): number {
         return orders.reduce((acc, order) => acc + order.total(), 0);
     }
