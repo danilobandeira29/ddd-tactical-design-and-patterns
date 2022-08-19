96c76c787736e75331a9d1b90e8d85609f411dd3
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-19 11:32:36 -0300

    feat: add product repository and implement create method

---
+++ src/infrastructure/repository/product.repository.ts
@@ -0,0 +1,25 @@
+import ProductRepositoryInterface from "../../domain/repository/product-repository.interface";
+import Product from "../../domain/entity/product";
+import ProductModel from "../db/sequelize/model/product.model";
+
+export default class ProductRepository implements ProductRepositoryInterface {
+    async create(entity: Product): Promise<void> {
+        await ProductModel.create({
+            id: entity.id,
+            name: entity.name,
+            price: entity.price
+        })
+    }
+
+    async update(entity: Product): Promise<void> {
+        throw new Error("Method not implemented");
+    }
+
+    async find(id: string): Promise<Product> {
+        throw new Error("Method not implemented");
+    }
+
+    async findAll(): Promise<Array<Product>> {
+        throw new Error("Method not implemented");
+    }
+}
