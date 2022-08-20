45bb95f29acf83ca8a52de78d333a844171e2849
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-20 14:01:30 -0300

    feat: add update method

--- src/infrastructure/repository/order.repository.ts
+++ src/infrastructure/repository/order.repository.ts
@@ -1,9 +1,9 @@
-import Customer from "../../domain/entity/customer";
 import Order from "../../domain/entity/order";
 import OrderModel from "../db/sequelize/model/order.model";
 import OrderItemModel from "../db/sequelize/model/order-item.model";
+import OrderRepositoryInterface from "../../domain/repository/order.repository.interface";
 
-export default class OrderRepository {
+export default class OrderRepository implements OrderRepositoryInterface {
     async create(entity: Order): Promise<void> {
         await OrderModel.create({
             id: entity.id,
@@ -20,15 +20,32 @@ export default class OrderRepository {
             { include: [{ model: OrderItemModel }]});
     }
 
-    async update(entity: Customer): Promise<void> {
+    async update(entity: Order): Promise<void> {
+        await OrderModel.destroy({
+            where: {
+                id: entity.id
+            }
+        });
+        await OrderModel.create({
+                id: entity.id,
+                customer_id: entity.customerId,
+                total: entity.total(),
+                items: entity.items.map(item => ({
+                    id: item.id,
+                    name: item.name,
+                    price: item.price,
+                    product_id: item.productId,
+                    quantity: item.quantity
+                }))
+            },
+            { include: [{ model: OrderItemModel }]});
+    }
+
+    async find(id: string): Promise<Order> {
         throw new Error("Method not implemented");
     }
 
-    async find(id: string): Promise<Customer> {
-        throw new Error("Method not implemented");
-    }
-
-    async findAll(): Promise<Array<Customer>> {
+    async findAll(): Promise<Array<Order>> {
         throw new Error("Method not implemented");
     }
 }
