6bb749b1f9646a5b0a975df6a4f27ccdb67c803f
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-23 10:21:04 -0300

    feat: add event dispatcher

---
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -0,0 +1,24 @@
+import EventDispatcherInterface from "./event-dispatcher.interface";
+import EventInterface from "./event.interface";
+import EventHandlerInterface from "./event-handler.interface";
+
+export default class EventDispatcher implements EventDispatcherInterface {
+    private eventHandlers: { [eventName: string]: Array<EventHandlerInterface> } = {};
+
+    get getEventHandlers(): { [eventName: string]: Array<EventHandlerInterface> } {
+        return this.eventHandlers;
+    }
+
+    notify(event: EventInterface): void {}
+
+    register(eventName: string, eventHandler: EventHandlerInterface): void {
+        if (!this.eventHandlers[eventName]) {
+            this.eventHandlers[eventName] = [];
+        }
+        this.eventHandlers[eventName].push(eventHandler);
+    }
+
+    unregister(eventName: string, eventHandler: EventHandlerInterface): void {}
+
+    unregisterAll(): void {}
+}
