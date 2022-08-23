e0a45ecf2f2b198c09260791cbedc411cec22208
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-23 11:50:51 -0300

    test: should notify all event handlers

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -1,5 +1,6 @@
 import EventDispatcher from "./event-dispatcher";
 import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+import ProductCreatedEvent from "../product/product-created.event";
 
 describe("Domain Events tests", () => {
     it("should register an event handler", () => {
@@ -32,5 +33,18 @@ describe("Domain Events tests", () => {
         eventDispatcher.unregisterAll();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeUndefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent2"]).toBeUndefined();
-    })
+    });
+
+    it("should notify all event handlers", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        const spyEventHandler = jest.spyOn(eventHandler, "handle");
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        const productCreatedEvent = new ProductCreatedEvent({
+            name: "Product 1",
+            price: 4000,
+        });
+        eventDispatcher.notify(productCreatedEvent);
+        expect(spyEventHandler).toHaveBeenCalledTimes(1);
+    });
 })
