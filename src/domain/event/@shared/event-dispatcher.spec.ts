5f34b191eb29ba49a83ff190b2bc5b0198202761
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-23 10:51:42 -0300

    test: should unregister an event handler

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -9,5 +9,15 @@ describe("Domain Events tests", () => {
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toEqual(1);
         expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toEqual(eventHandler);
-    })
+    });
+
+    it("should unregister an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"][0]).toEqual(eventHandler);
+        eventDispatcher.unregister("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toEqual(0);
+    });
 })
