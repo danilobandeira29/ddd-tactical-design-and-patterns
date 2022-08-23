559b72c9305d134c59ebea473ac489c9403a67cf
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-23 10:21:47 -0300

    test: should register an event handler

---
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -0,0 +1,12 @@
+import EventDispatcher from "./event-dispatcher";
+import SendEmailWhenProductIsCreatedHandler from "../product/handler/send-email-when-product-is-created.handler";
+
+describe("Domain Events tests", () => {
+    it("should register an event handler", () => {
+        const eventDispatcher = new EventDispatcher();
+        const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+        eventDispatcher.register("ProductCreatedEvent", eventHandler);
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"]).toBeDefined();
+        expect(eventDispatcher.getEventHandlers["ProductCreatedEvent"].length).toEqual(1);
+    })
+})
