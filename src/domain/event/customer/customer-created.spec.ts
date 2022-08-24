242df0ec02906783d8e42a1ef6e53fc84b662d2c
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-24 08:52:24 -0300

    test: should console log a message when address of a customer is changed

--- src/domain/event/customer/customer-created.spec.ts
+++ src/domain/event/customer/customer-created.spec.ts
@@ -3,6 +3,9 @@ import EnviaConsoleLog1Handler from "./handler/envia-console-log-1.handler";
 import EnviaConsoleLog2Handler from "./handler/envia-console-log-2.handler";
 import Customer from "../../entity/customer";
 import CustomerCreatedEvent from "./customer-created.event";
+import Address from "../../entity/address";
+import LogWhenCustomerChangeAddressHandler from "./handler/log-when-customer-change-address.handler";
+import CustomerAddressChangedEvent from "./customer-address-changed.event";
 
 describe("Customer created tests", () => {
    it("should console log two messages when customer is created", () => {
@@ -15,7 +18,26 @@ describe("Customer created tests", () => {
       new Customer("1", "Danilo Bandeira");
       const customerCreatedEvent = new CustomerCreatedEvent({});
       eventDispatcher.notify(customerCreatedEvent);
+      expect(spyConsole).toHaveBeenCalledTimes(2);
       expect(spyConsole).toHaveBeenNthCalledWith(1, "Esse é o primeiro console.log do evento: CustomerCreated")
       expect(spyConsole).toHaveBeenNthCalledWith(2, "Esse é o segundo console.log do evento: CustomerCreated");
    });
+
+   it("should console log a message when address of a customer is changed", () => {
+      const eventDispatcher = new EventDispatcher();
+      const eventHandler = new LogWhenCustomerChangeAddressHandler();
+      const spyConsole = jest.spyOn(console, "log");
+      eventDispatcher.register("CustomerAddressChangedEvent", eventHandler);
+      const customer = new Customer("1", "Danilo Bandeira");
+      const address = new Address("Street", "1", "00000000", "City");
+      customer.changeAddress(address);
+      const customerAddressChangedEvent = new CustomerAddressChangedEvent({
+         id: customer.id,
+         nome: customer.name,
+         endereco: customer.address?.toString()
+      });
+      eventDispatcher.notify(customerAddressChangedEvent);
+      expect(spyConsole).toHaveBeenCalledTimes(1);
+      expect(spyConsole).toHaveBeenNthCalledWith(1, "Endereço do cliente: 1, Danilo Bandeira alterado para: Street, 1, 00000000, City");
+   })
 });
