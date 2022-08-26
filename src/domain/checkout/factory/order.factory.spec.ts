8a9060706756bf0576091ae32882b734f599f14a
Danilo Bandeira <danilo.bandeira@growthtech.com.br>
2022-08-26 13:15:04 -0300

    test: should create an order

---
+++ src/domain/checkout/factory/order.factory.spec.ts
@@ -0,0 +1,24 @@
+import { v4 as uuid } from "uuid";
+import OrderFactory from "./order.factory";
+
+describe("Order Factory unit tests", () => {
+   it("should create an order", () => {
+      const orderProps = {
+         id: uuid(),
+         customerId: uuid(),
+         items: [
+            {
+               id: uuid(),
+               name: "Item 1",
+               productId: uuid(),
+               quantity: 1,
+               price: 4000
+            }
+         ]
+      };
+      const order = OrderFactory.create(orderProps);
+      expect(order.id).toEqual(orderProps.id);
+      expect(order.customerId).toEqual(orderProps.customerId);
+      expect(order.items.length).toEqual(1);
+   });
+});
